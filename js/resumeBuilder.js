bede2fc9bcbe0fec9a827c13e58cb58cf611f6a1
Jooyong Chung <Jooyong Chung>
2016-03-08 02:21:11 -0800

    Added JSON objects to resumeBuilder.js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,70 @@
+var bio = {
+	"name": "Jooyong Chung",
+	"role": "Web Developer",
+	"contact": {
+		"mobile": "111-111-1337",
+		"email": "jychung0801@gmail.com",
+		"github": "KingArpy",
+		"location": "Bellingham, WA"
+	},
+	"welcomeMessage": "Welcome to my resume!(FEND Project 2)",
+	"skills": ["fight crime", "play video games", "html/css/javascript"],
+	"biopic": "images/fry.jpg"
+};
+
+var education = {
+	"schools": [
+		{
+			"name": "Western Washington University",
+			"location": "Bellingham, WA",
+			"degree": "none",
+			"majors": "n/a",
+			"dates": "2 years",
+			"url": "http://www.wwu.edu/"
+		},
+		{
+			"name": "Whatcom Community College",
+			"location": "Bellingham, WA",
+			"degree": "none",
+			"majors": "n/a",
+			"dates": "2 quarters",
+			"url": "http://www.whatcom.edu/"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Front-end Web Development",
+			"school": "Udacity",
+			"date": "1 Month",
+			"url": "http://www.udacity.com/"
+		}
+	]
+};
+
+var work = {
+	"jobs": [
+		{
+			"employer": "fake employer 1",
+			"title": "fake title 1",
+			"location": "Seoul, South Korea",
+			"dates": "1 year",
+			"description": "this is a first fake job description necessary to complete the project. Location is Seoul, South Korea."
+		},
+		{
+			"employer": "fake employer 2",
+			"title": "fake title 2",
+			"location": "Saint Louis, MO",
+			"dates": "6 months",
+			"description": "this is a second fake job description. Location is based on Saint Louis."
+		}
+	]
+};
+
+var projects = {
+	"projects": [{
+			"title": "project 1",
+			"dates": "February 2016",
+			"description": "This is the first Front-end Web Development project that I have completed",
+			"images": "insert-image-url-here"
+		}]
+}
